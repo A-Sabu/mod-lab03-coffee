7ce04077791ca58f05d5dcf8308edbd9d44d6c8a
A-Sabu <a.saburova04@mail.ru>
2025-04-05 20:55:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include "Automata.h"
 
 int main(int argc, char** argv) {
+    
+	vector<string> menu = { "Capuchino", "Expresso", "Raf" };
+	vector<float> prices = { 1.5, 2.5, 5.5 };
+
+	Automata coffeeMachine(menu, prices);
+
+	coffeeMachine.on();
+	coffeeMachine.coin(6);
+	coffeeMachine.choice(2);
+	coffeeMachine.check();
 
     return 1;
+    
 }
