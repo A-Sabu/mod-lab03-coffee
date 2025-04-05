c146245686f3547e926891fca4af70373d324e5c
A-Sabu <a.saburova04@mail.ru>
2025-04-05 23:25:14 +0300

    Update tests.cpp
    Trial test

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,15 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(task1, test1) {
+    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
+    vector<float> prices = { 1.5, 2.5, 5.5 };
+
+    Automata coffeeMachine(menu, prices);
+    
+    coffeeMachine.on();
+    
+    string result = coffeeMachine.getState();
+    EXPECT_EQ("WAIT", result);
+  }
