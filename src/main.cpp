f0d6ef0789e5d97284303bc0ab07d888bad107b9
A-Sabu <a.saburova04@mail.ru>
2025-04-06 22:00:53 +0300

    Update main.cpp - cpplint

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
+// Copyright 2025 A-Sabu
 #include "Automata.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
 int main(int argc, char** argv) {
-    
-	vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-	vector<float> prices = { 1.5, 2.5, 5.5 };
 
-	Automata coffeeMachine(menu, prices);
+        vector<string> menu = { "Capuchino", "Expresso", "Raf" };
+        vector<float> prices = { 1.5, 2.5, 5.5 };
 
-	coffeeMachine.on();
-	coffeeMachine.coin(6);
-	coffeeMachine.choice(2);
-	coffeeMachine.check();
+        Automata coffeeMachine(menu, prices);
 
-    return 1;
-    
+        coffeeMachine.on();
+        coffeeMachine.coin(6);
+        coffeeMachine.choice(2);
+        coffeeMachine.check();
+
+        return 1;
 }
