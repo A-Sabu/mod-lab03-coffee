56a1ff0b312b21d978482590d9049a555e423062
A-Sabu <a.saburova04@mail.ru>
2025-04-06 22:23:57 +0300

    Update tests.cpp - + std::

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,33 +6,33 @@
 #include <string>
 
 TEST(task_state, test1) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
     coffeeMachine.on();
 
-    string result = coffeeMachine.getState();
+    std::string result = coffeeMachine.getState();
     EXPECT_EQ("WAIT", result);
 }
 
 TEST(task_state, test2) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
     coffeeMachine.on();
     coffeeMachine.coin(3);
 
-    string result = coffeeMachine.getState();
+    std::string result = coffeeMachine.getState();
     EXPECT_EQ("ACCEPT", result);
 }
 
 TEST(task_state, test3) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
@@ -40,13 +40,13 @@ TEST(task_state, test3) {
     coffeeMachine.coin(3);
     coffeeMachine.choice(1);
 
-    string result = coffeeMachine.getState();
+    std::string result = coffeeMachine.getState();
     EXPECT_EQ("CHECK", result);
 }
 
 TEST(task_state, test4) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
@@ -55,13 +55,13 @@ TEST(task_state, test4) {
     coffeeMachine.choice(1);
     coffeeMachine.check();
 
-    string result = coffeeMachine.getState();
+    std::string result = coffeeMachine.getState();
     EXPECT_EQ("WAIT", result);
 }
 
 TEST(task_state, test5) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
@@ -71,26 +71,26 @@ TEST(task_state, test5) {
     coffeeMachine.check();
     coffeeMachine.off();
 
-    string result = coffeeMachine.getState();
+    std::string result = coffeeMachine.getState();
     EXPECT_EQ("OFF", result);
 }
 
 TEST(task_state, test6) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
     coffeeMachine.on();
     coffeeMachine.on();
 
-    string result = coffeeMachine.getState();
+    std::string result = coffeeMachine.getState();
     EXPECT_EQ("WAIT", result);
 }
 
 TEST(task_state, test7) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
@@ -98,13 +98,13 @@ TEST(task_state, test7) {
     coffeeMachine.coin(1);
     coffeeMachine.cancel();
 
-    string result = coffeeMachine.getState();
+    std::string result = coffeeMachine.getState();
     EXPECT_EQ("WAIT", result);
 }
 
 TEST(task_state, test8) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
@@ -113,13 +113,13 @@ TEST(task_state, test8) {
     coffeeMachine.choice(0);
     coffeeMachine.cancel();
 
-    string result = coffeeMachine.getState();
+    std::string result = coffeeMachine.getState();
     EXPECT_EQ("WAIT", result);
 }
 
 TEST(task_state, test9) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
@@ -127,13 +127,13 @@ TEST(task_state, test9) {
     coffeeMachine.coin(1);
     coffeeMachine.coin(2);
 
-    string result = coffeeMachine.getState();
+    std::string result = coffeeMachine.getState();
     EXPECT_EQ("ACCEPT", result);
 }
 
 TEST(task_state, test10) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
@@ -142,13 +142,13 @@ TEST(task_state, test10) {
     coffeeMachine.choice(2);
     coffeeMachine.check();
 
-    string result = coffeeMachine.getState();
+    std::string result = coffeeMachine.getState();
     EXPECT_EQ("WAIT", result);
 }
 
 TEST(task_state, test11) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
@@ -156,13 +156,13 @@ TEST(task_state, test11) {
     coffeeMachine.coin(2);
     coffeeMachine.check();
 
-    string result = coffeeMachine.getState();
+    std::string result = coffeeMachine.getState();
     EXPECT_EQ("ACCEPT", result);
 }
 
 TEST(task_cash, test1) {
-    vector<string> menu = { "Capuchino", "Expresso", "Raf" };
-    vector<float> prices = { 1.5, 2.5, 5.5 };
+    std::vector<std::string> menu = { "Capuchino", "Expresso", "Raf" };
+    std::vector<float> prices = { 1.5, 2.5, 5.5 };
 
     Automata coffeeMachine(menu, prices);
 
