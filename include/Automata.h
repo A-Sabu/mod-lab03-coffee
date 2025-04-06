d382178d956ddce262fc063d851619f2734badaa
A-Sabu <a.saburova04@mail.ru>
2025-04-06 21:30:21 +0300

    Update Automata.h - cpplint

--- include/Automata.h
+++ include/Automata.h
@@ -1,36 +1,30 @@
+// Copyright 2025 A-Sabu
 #pragma once
 #include <iostream>
 #include <vector>
-
-using namespace std;
+#include <string>
 
 class Automata {
-public:
-	Automata(vector<string> menu, vector<float> prices);
-	void on();
-	void off();
-	void coin(float cash_now);
-	vector<string> getMenu();
-	void printMenu();
-	string getState();
-	void choice(int user_choice);
-	void check();
-	void cancel();
-	void cook();
-	void finish();
-	float giveChange();
-private:
-	enum STATES {
-		OFF,
-		WAIT,
-		ACCEPT,
-		CHECK,
-		COOK
-	};
-	float _cash;
-	vector<string> _menu;
-	vector<float> _prices;
-	STATES _state;
-	float _user_cash;
-	int _user_choice;
+ public:
+    Automata(std::vector<std::string> menu, std::vector<float> prices);
+    void on();
+    void off();
+    void coin(float cash_now);
+    std::vector<std::string> getMenu();
+    void printMenu();
+    std::string getState();
+    void choice(int user_choice);
+    void check();
+    void cancel();
+    void cook();
+    void finish();
+    float giveChange();
+ private:
+    enum STATES {OFF, WAIT, ACCEPT, CHECK, COOK};
+    float _cash;
+    std::vector<std::string> _menu;
+    std::vector<float> _prices;
+    STATES _state;
+    float _user_cash;
+    int _user_choice;
 };
